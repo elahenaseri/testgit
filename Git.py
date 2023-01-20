964f01a72e7381b9c45d7d3fb8f66e23bb5ceca6
elahenaseri <123113584+elahenaseri@users.noreply.github.com>
2023-01-20 12:40:25 +0330

    Add files via upload

---
+++ Git.py
@@ -0,0 +1,18 @@
+x="hi how are you Friend"
+print(x)
+num_h=len(x.split(" "))
+num_k=len(x)
+a=x.replace('r','p')
+print("num_h=",num_h)
+print("num_k=",num_k)
+print("Replace=", a)
+
+print()
+
+
+q="hi how are you ?, I am fine., Whats up?, the health."
+num_j=len(q.split(","))
+print(q)
+print("num_j=",num_j)
+
+
